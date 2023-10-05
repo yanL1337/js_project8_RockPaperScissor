26521c9143e82ae1f2512953df87840f648c358e
Yan Luka Keita <yanlukakeita2000@gmail.com>
2023-10-05 10:08:34 +0200

    animations etc

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,50 +26,57 @@ lange.forEach((elt) => {
 
 auswahl.forEach((elt) => {
   elt.addEventListener("click", () => {
-    event.preventDefault;
-    count++;
-    if (count > rounds) {
-      alert(
-        `GAME OVER Winner ist der ${
-          pointsPlayer > pointsComp ? "Spieler" : "Computer"
-        }`
-      );
+    if (rounds !== 0) {
+      event.preventDefault;
+      count++;
+      if (count > rounds) {
+        alert(
+          `GAME OVER Winner ist der ${
+            pointsPlayer > pointsComp ? "Spieler" : "Computer"
+          }`
+        );
+        window.location.replace("index.html");
+      }
+      compTurn = Math.floor(Math.random() * 3);
+      radios.classList.add("hide");
+      runden.classList.remove("hide");
+
+      rundeFrom.innerHTML = count;
+      rundeTill.innerHTML = rounds;
+
+      playerChoose.innerHTML = elt.innerHTML;
+      compChoose.innerHTML = items[compTurn];
+
+      switch (true) {
+        case elt.textContent.trim() === items[compTurn]:
+          pointsComp++;
+          pointsPlayer++;
+          commentaryText = `${elt.textContent.trim()} und ${
+            items[compTurn]
+          } ergibt unentschieden`;
+
+          break;
+
+        case elt.textContent.trim() === "✋" && items[compTurn] === "✊":
+        case elt.textContent.trim() === "✌️" && items[compTurn] === "✋":
+        case elt.textContent.trim() === "✊" && items[compTurn] === "✌️":
+          pointsPlayer++;
+          commentaryText = `${elt.textContent.trim()} Beats ${items[compTurn]}`;
+          break;
+
+        default:
+          pointsComp++;
+          commentaryText = `${
+            items[compTurn]
+          } Beats ${elt.textContent.trim()} `;
+          break;
+      }
+      commentary.innerHTML = commentaryText;
+      cPoints.innerHTML = pointsComp;
+      pPoints.innerHTML = pointsPlayer;
+    } else {
+      alert("Bitte runden angeben...");
       window.location.replace("index.html");
     }
-    compTurn = Math.floor(Math.random() * 3);
-    radios.classList.add("hide");
-    runden.classList.remove("hide");
-
-    rundeFrom.innerHTML = count;
-    rundeTill.innerHTML = rounds;
-
-    playerChoose.innerHTML = elt.innerHTML;
-    compChoose.innerHTML = items[compTurn];
-
-    switch (true) {
-      case elt.textContent.trim() === items[compTurn]:
-        pointsComp++;
-        pointsPlayer++;
-        commentaryText = `${elt.textContent.trim()} und ${
-          items[compTurn]
-        } ergibt unentschieden`;
-
-        break;
-
-      case elt.textContent.trim() === "✋" && items[compTurn] === "✊":
-      case elt.textContent.trim() === "✌️" && items[compTurn] === "✋":
-      case elt.textContent.trim() === "✊" && items[compTurn] === "✌️":
-        pointsPlayer++;
-        commentaryText = `${elt.textContent.trim()} Beats ${items[compTurn]}`;
-        break;
-
-      default:
-        pointsComp++;
-        commentaryText = `${items[compTurn]} Beats ${elt.textContent.trim()} `;
-        break;
-    }
-    commentary.innerHTML = commentaryText;
-    cPoints.innerHTML = pointsComp;
-    pPoints.innerHTML = pointsPlayer;
   });
 });
